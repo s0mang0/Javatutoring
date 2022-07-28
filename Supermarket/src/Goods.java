62f6ed110519f8b8d4f95319e4c2a4cb6ee4dd91
unknown <eunseo0222@gmail.com>
2022-07-28 13:05:01 +0900

    add menu

---
+++ Supermarket/src/Goods.java
@@ -0,0 +1,35 @@
+public class Goods {
+    public int id;
+    public String name;
+    public String pDate;
+    public int price;
+    public float discount;
+
+    Goods() {
+        this(0, "비어있음", "0000/00/00", 0 ,0);
+    }
+
+    Goods(int id , String name, int price){
+        this(id, name, "0000/00/00", price,0);
+    }
+
+    Goods(int id, String name, String pDate, int price, float discount) {
+      this.id = id;
+      this.name = name;
+      this.pDate = pDate;
+      this.price = price;
+      this.discount = discount;
+    }
+
+
+
+    public void inform(){
+        System.out.println("상품번호: " + this.id + " | 상품명: " + name + " | 가격: " + price);
+        System.out.println("생산일자: " + pDate + "| 할인율 : " + discount);
+        System.out.println("--------------------------------------------------");
+    }
+
+    public int discountPrice() {
+        return (int) (price - price * (discount / 100));
+    }
+}
--- Supermarket/src/Supermarket.java
+++ Supermarket/src/Supermarket.java
@@ -3,67 +3,58 @@ import java.util.Scanner;
 
 public class Supermarket {
     public static void main(String[] args) {
-        System.out.println("은서마켓에 온 것을 환영합니다 ~~!");
+        System.out.println("은서마켓 온 것을 환영합니다~~!");
         Scanner scanner = new Scanner(System.in);
-        String goods[] = new String[5];
-        int price[] = new int[5];
-        int index = 1;
-        boolean onOFF = true;
-        int menu = 4;
-        goods[0] ="삼다수";
-        price[0] = 500;
+        Goods[] goodsArr = new Goods[5];
+        int index = 0;
+        boolean onOff = true;
 
-        while(onOFF) {
-            try {
-                menu = 0;
-                System.out.println("\n1 . 상품등록 / 2. 상품조회 / 3. 결제 / 4.사용종료");
-                System.out.print("메뉴를 선택하세요. >> ");
-                menu = scanner.nextInt();
-            }
-            catch (InputMismatchException e) {
-                System.out.println("숫자만 입력해주세요.");
-                scanner.next();
-            }
-
-            switch (menu) {
-                case 1:
-                    if(index >= 5){
-                        System.out.println("등록 칸이 다 찼습니다.");
-                        continue;}
-                    else {
-                        System.out.print("상품을 등록해주세요>>");
-                        goods[index] = scanner.next();
-                        System.out.print(goods[index] + "원가를 입력해주세요 >>");
-                        price[index] = scanner.nextInt();
-                        index++;
-                    }
-                    break;
-                case 2:
-                    for(int i = 0; i<index; i++){ // index price.
-                        System.out.println("상품 ID: " + i  + "|" + goods[i] + "의 원가는 " + price[i] + "원 입니다.");
-                    }
-                    break;
-                case 3:
-                    System.out.println("상품ID를 입력해주세요");
-                    int select = scanner.nextInt();
-                    if(select >= index) {
-                        System.out.println("해당하신 물품은 존재하지 않습니다.");
-                        break;
-                    }
-                    else{
-                        System.out.print("할인율을 입력해주세요.");
-                        float discount = scanner.nextFloat();
-                        int dprice = (int)(price[select]*(100-discount)/100);
-                        System.out.println(goods[select] + "의 가격은 " + dprice + "원 입니다.");
-                    }
-
-                case 4:
-                    onOFF = false;
-                    break;
-            }
+        for (int n = 0; n < goodsArr.length; n++) {
+            goodsArr[n] = new Goods();
         }
 
-        System.out.println("이용해주셔서 감사합니다.");
-        scanner.close();
+        while (onOff) {
+
+            System.out.println("1. 상품등록 | 2. 상품조회 | 3. 계산 | 4. 사용종료");
+            System.out.print("메뉴를 선택해주세요.>> ");
+            try {
+                int menu = scanner.nextInt();
+                switch (menu) {
+                    case 1:
+                        System.out.print("상품명을 입력해주세요.>> ");
+                        String name = scanner.next();
+                        System.out.print("생산일자를 입력해주세요.>> ");
+                        String pDate = scanner.next();
+                        System.out.print("가격을 입력해주세요.>> ");
+                        int price = scanner.nextInt();
+                        if (pDate.equals("0")) {
+                            goodsArr[index % 5] = new Goods(index, name, price);
+                        } else {
+                            System.out.print("할인율을 입력해주세요. >>");
+                            float discount = scanner.nextFloat();
+                            goodsArr[index % 5] = new Goods(index, name, pDate, price, discount);
+                        }
+                        index++;
+                        break;
+                    case 2:
+                        System.out.println("--------------------------------------------------");
+                        for (int n = 0; n < goodsArr.length; n++) {
+                            goodsArr[n].inform();
+                        }
+                        break;
+                    case 3:
+                        System.out.print("상품번호를 입력해주세요.>> ");
+                        int i = scanner.nextInt();
+                        System.out.println(goodsArr[i].name + "의 가격은 " + goodsArr[i].discountPrice() + "원입니다.");
+                        break;
+                    case 4:
+                        System.out.println("이용해주셔서 감사합니다.");
+                        onOff = false;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("형식에 맞지 않습니다.");
+                scanner.next();
+            }
+        }
     }
 }
